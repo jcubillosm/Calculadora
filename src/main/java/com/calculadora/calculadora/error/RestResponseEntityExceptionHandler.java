d93b6e65de319ee0f7d07d36d8fc9deecfb41408
Jenniffer Cubillos <jcubillos@atsistemas.com>
2023-02-23 16:55:00 +0100

    Excepciones modificadas

--- src/main/java/com/calculadora/calculadora/error/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/calculadora/calculadora/error/RestResponseEntityExceptionHandler.java
@@ -1,28 +1,32 @@
 package com.calculadora.calculadora.error;
-
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.calculadora.calculadora.exceptions.mathOperationException;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RestController
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler {
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
-	protected ResponseEntity<ExceptionResponse> handlerAllException(Exception ex, WebRequest request) {
-		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
-				request.getDescription(false));
-		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	protected ResponseEntity<Object> handlerAllException(final RuntimeException ex, final WebRequest request) {
+	    log.error(ex.getMessage(), ex);
+	    return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR,
+	        request);
 	}
 	
-	@ExceptionHandler(UnsuportedMathOperationExeception.class)
-	public final ResponseEntity<ExceptionResponse> handlerBadRequestException(Exception ex, WebRequest request) {
-		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
-				request.getDescription(false));
-		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	@ExceptionHandler(mathOperationException.class)
+	public final ResponseEntity<Object> handlerBadRequestException(final RuntimeException ex, final WebRequest request){
+		log.error(ex.getMessage(), ex);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND,request);
 	}
 }
