8832f7c7e780dfe94a011ed5f66cedd5054c69ff
Jenniffer Cubillos <jcubillos@atsistemas.com>
2023-02-24 09:17:30 +0100

    Tracer añadido

--- src/main/java/com/calculadora/calculadora/controllers/CalculadoraController.java
+++ src/main/java/com/calculadora/calculadora/controllers/CalculadoraController.java
@@ -1,6 +1,7 @@
 package com.calculadora.calculadora.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.calculadora.calculadora.services.CalculadoraService;
 
+import io.corp.calculator.TracerImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
@@ -18,14 +20,18 @@ import lombok.extern.slf4j.Slf4j;
 public class CalculadoraController {
 	@Autowired
 	CalculadoraService calculadoraService;
+	private TracerImpl tracer = new TracerImpl();
 
 	@Operation(summary = "Calculadora")
 	@GetMapping("/{operation}/{firstOperator}/{secondOperator}")
-	public Double operationControl(@Parameter(description = "Operación seleccionada") @PathVariable String operation,
+	public ResponseEntity<?>  operationControl(@Parameter(description = "Operación seleccionada") @PathVariable String operation,
 			@Parameter(description = "Primer operador") @PathVariable String firstOperator,
 			@Parameter(description = "Segundo operador") @PathVariable String secondOperator) {
 		log.info("CalculadoraController: operationControl");
-		return calculadoraService.operationControl(operation, firstOperator, secondOperator);
+		Double result = calculadoraService.operationControl(operation, firstOperator, secondOperator);
+		
+		tracer.trace(result);
+		return ResponseEntity.ok(result);
 	}
 
 }
